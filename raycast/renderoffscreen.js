947d2004f1905bee332b021a67bb7a29b8e862bc
danerjin <66403614+danerjin@users.noreply.github.com>
2023-11-03 17:48:03 -0700

    Update renderoffscreen.js

--- raycast/renderoffscreen.js
+++ raycast/renderoffscreen.js
@@ -49,6 +49,9 @@ function playsound(src,mul=1) {
 }
 gameIsOn=false;
 textscreen='Get Ready!';
+function rgb(csscolor){
+	return 'rgb('+csscolor[0]+','+csscolor[1]+','+csscolor[2]+')';
+}
 var Sprite = function(x,y,texture,block,hitbox,h,z,vmove){
   this.x = x;
   this.y = y;
@@ -173,7 +176,7 @@ var Enemy = function(x,y,z,texture,hp,rot,speed,dmg,melee,cool,burst,flinch,weap
 					}
 				}
 			}else{
-				if(dist<(player.isCrouching?8:15)&&canSee(this)){
+				if(dist<(player.isCrouching?4:10)&&canSee(this)){
 					this.alert = true;
 				}else{
 					this.alert = false;
@@ -310,22 +313,25 @@ var WallStripe = function(front,backdist){
 		if(this.front.y > this.backy&&this.backy<=screenHeight){
 			//topside
 			var drawEnd = Math.min(this.front.y,screenHeight);
-			//drawFillRectangle(this.front.x,this.backy,stripWidth,this.front.y-this.backy,fill="#69FF00");// <= this was temporary
-			for(var y = this.backy; y<=drawEnd; y+=stripWidth){
-				// Current y position compared to the center of the screen (the horizon)
-				var p = y - screenHeight / 2 - player.pitch;
-				// Vertical position of the camera.
-				// Horizontal distance from the camera to the floor for the current row.
-				// 0.5 is the z position exactly in the middle between floor and ceiling.
-				var rowDistance = (posZ-heightMap[this.wallY][this.wallX]*screenHeight) / (p);
-		    var floorX = player.x + rowDistance * (dirX+(this.x/stripWidth-numRays/2)*planeX/(numRays/2));
-		    var floorY = player.y + rowDistance * (dirY+(this.x/stripWidth-numRays/2)*planeY/(numRays/2));
-	      // get the texture coordinate from the fractional part
-	      var tx = floorX%1;
-	      var ty = floorY%1;
-	      var floorTexture=this.front.tex;
-	      // floor drawing
-	      drawCeilRectangle(this.x,y,stripWidth,stripWidth,tx,ty,floorTexture);
+			if(floor){
+				for(var y = this.backy; y<=drawEnd; y+=stripWidth){
+					// Current y position compared to the center of the screen (the horizon)
+					var p = y - screenHeight / 2 - player.pitch;
+					// Vertical position of the camera.
+					// Horizontal distance from the camera to the floor for the current row.
+					// 0.5 is the z position exactly in the middle between floor and ceiling.
+					var rowDistance = (posZ-heightMap[this.wallY][this.wallX]*screenHeight) / (p);
+			    var floorX = player.x + rowDistance * (dirX+(this.x/stripWidth-numRays/2)*planeX/(numRays/2));
+			    var floorY = player.y + rowDistance * (dirY+(this.x/stripWidth-numRays/2)*planeY/(numRays/2));
+		      // get the texture coordinate from the fractional part
+		      var tx = floorX%1;
+		      var ty = floorY%1;
+		      var floorTexture=this.front.tex;
+		      // floor drawing
+		      drawCeilRectangle(this.x,y,stripWidth,stripWidth,tx,ty,floorTexture);
+				}
+			}else{
+			 	drawFillRectangle(this.front.x,this.backy,stripWidth,this.front.y-this.backy,fill=rgb(wallTypes[this.front.tex]));// <= this was temporary
 			}
 		}
 		if(this.front.y<=screenHeight){this.front.draw();}
@@ -520,7 +526,7 @@ var enemies = [
 	new Enemy(4.5,7.5,0,"guard",75,3*Math.PI/2,0.02,0,false,0.5,1,true,1),
 	new Enemy(4.5,15.5,0,"ss",100,3*Math.PI/2,0.035,0,false,0.4,3,true,2),
 ];
-var sounds=['swsh_0_0','weapon_3','weapon_2','weapon_7'];
+var sounds=[['swsh_0_0','swsh_0_1'],'weapon_3','weapon_2','weapon_7'];
 var pickups = [
 	new Pickup(14,1.6,"smg",0,0),
 	new Pickup(6.3,9,"chaingun",0,0),
@@ -537,7 +543,8 @@ window.mobileAndTabletCheck = function() {
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
-var wallTextures = []
+var wallTextures = [];
+var wallTypes = [0,[0,0,88],[106,0,0],[147, 147, 147],[108,69,39],[180, 179, 82],[89,0,90],[0,142,142],[0,142,142],[202,202,202],[124,124,124],[108,69,39]];
 for(var texture = 1; texture < 12;texture++){
     var wallTexture = new Image();
   	wallTexture.crossOrigin = "Anonymous";
@@ -632,8 +639,14 @@ var player = {
 	lives:5,
 	timer:0,
 	regen:0,
+	atkount:0,
+	score:0,
 	fire:function(stripe){
-		playsound('weapons/'+sounds[this.weapon]);
+		if(this.weapon===0){
+			playsound('weapons/'+sounds[this.weapon][this.atkount%2]);
+		}else{
+			playsound('weapons/'+sounds[this.weapon]);
+		}
 		if(this.weapon === 0){
 			enemies.forEach(enemy => function(enemy){
 				if(((enemy.x-player.x)**2+(enemy.y-player.y)**2)**0.5 < player.range[player.weapon]/24 && enemy.hp!==0){
@@ -942,6 +955,7 @@ function gameCycle() {
 						if(newthing==3){
 							//fire knife
 							player.fire(centerStripe);
+							player.atkount++;
 						}
 					}else if(player.weapon===1){
 						if(newthing===2){
