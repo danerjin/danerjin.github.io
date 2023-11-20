c84dd7d229dcab8732644e726c3db0bcd38d5b76
danerjin <66403614+danerjin@users.noreply.github.com>
2023-11-20 15:42:48 -0800

    Update renderoffscreen.js

--- raycast/renderoffscreen.js
+++ raycast/renderoffscreen.js
@@ -563,110 +563,6 @@ var ceilinglayout = [
   [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
   [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
 ];
-var sprites = [
-  new Sprite(21.5,10,"lamp",false,0.6,0,0,0),
-  new Sprite(18,3,"lamp",false,0.6,0,0,0),
-  new Sprite(9.5,3.5,"lamp",false,0.6,0,0,0),
-  new Sprite(9.5,10,"lamp",false,0.6,0,0,0),
-  new Sprite(3,3.5,"lamp",false,0.6,0,0,0),
-  new Sprite(3,19.5,"lamp",false,0.6,0,0,0),
-  new Sprite(3,13,"lamp",false,0.6,0,0,0),
-  new Sprite(14,19,"lamp",false,0.6,0,0,0),
-  new Sprite(2,8,"pillar",true,0.6,1,0,0),
-  new Sprite(2,9,"pillar",true,0.6,1,0,0),
-  new Sprite(2,10,"pillar",true,0.6,1,0,0),
-  new Sprite(21,2,"barrel",true,0.6,0.4,0,0),
-  new Sprite(15.73,2.1,"barrel",true,0.6,0.4,0,0),
-  new Sprite(15.27,2.1,"barrel",true,0.6,0.4,0,0),
-  new Sprite(15.5,2.3,"barrel",true,0.6,0.4,0.4,0.4),
-  new Sprite(15.5,2.7,"barrel",true,0.6,0.4,0,0),
-  new Sprite(3,2,"barrel",true,0.6,0.4,0,0),
-  new Sprite(9,10,"barrel",true,0.6,0.4,0,0),
-  new Sprite(9.5,10,"barrel",true,0.6,0.4,0,0),
-  new Sprite(10,10,"barrel",true,0.6,0.4,0,0)
-];
-var enemies = [
-	new Enemy(15.5,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(16.0,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(16.5,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(17.0,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(15.5,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(16.0,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(16.5,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(17.0,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
-	new Enemy(4.5,7.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(4.5,15.5,0,"ss",100,3*Math.PI/2,0.035,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
-	new Enemy(5.5,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(6.0,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(6.5,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(7.0,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(5.5,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(6.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(6.5,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(7.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(10.0,20.0,0,"guard",50,Math.PI,0.075,0,true,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(11.0,20.0,0,"guard",50,Math.PI,0.075,0,true,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(10.0,21.0,0,"ss",100,Math.PI,0.075,0,true,0.5,1,true,2,[['ammo',2],['smg',0]]),
-	new Enemy(11.0,21.0,0,"ss",100,Math.PI,0.075,0,true,0.5,1,true,2,[['ammo',2],['smg',0]]),
-	new Enemy(7.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
-	new Enemy(21.5,8.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(19.5,8.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(21.5,6.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(19.5,6.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
-	new Enemy(21.0,15.5,0,"ss",100,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
-	new Enemy(20.0,15.5,0,"ss",100,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
-	new Enemy(19.0,15.5,0,"ss",500,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0],['bluekey',1]]),
-];
-var sounds=[['swsh_0_0','swsh_0_1'],'weapon_3','weapon_2','weapon_26'];
-var pickups = [
-	new Pickup(15.5,7.5,'chaingun',0,0,0),
-	new Pickup(15.5,7.5,'lives',0,0,4),
-	new Pickup(15.5,7.5,'goldkey',0,0,1),
-	new Pickup(15.5,3.5,'backpack',0,0,5),
-];
-var weapons_imgs = [];
-for(var texture = 0; texture < weapon_names.length;texture++){
-    var weaponTexture = new Image();
-  	weaponTexture.crossOrigin = "Anonymous";
-  	weaponTexture.src = `sprites/weapons/${weapon_names[texture]}.png`;
-    weapons_imgs.push(weaponTexture);
-}
-window.mobileAndTabletCheck = function() {
-  let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
-};
-var wallTextures = [];
-var wallTypes = [0,[0,0,88],[106,0,0],[147, 147, 147],[108,69,39],[180, 179, 82],[89,0,90],[0,142,142],[0,142,142],[202,202,202],[124,124,124],[108,69,39]];
-for(var texture = 1; texture < 12;texture++){
-    var wallTexture = new Image();
-  	wallTexture.crossOrigin = "Anonymous";
-  	wallTexture.src = `walls_${texture}.png`;
-    wallTextures.push(wallTexture);
-}
-var floorTextures = []
-for(var texture = 1; texture < 11;texture++){
-    var floorTexture = new Image();
-  	floorTexture.crossOrigin = "Anonymous";
-  	floorTexture.src = `floor_${texture}.png`;
-    floorTextures.push(floorTexture);
-}
-//var floorData=getimagedata(floorTextures[1]);
-var weapon_size = screenWidth*2/3;
-mobile = window.mobileAndTabletCheck();
-var canvas = $("screen");
-var screenWidth = canvas.width;
-var screenHeight = canvas.height;
-var pickupIsPresent = false;
-var pickupNum;
-var offcanvas = new OffscreenCanvas(screenWidth,screenHeight);//document.createElement('canvas');//
-var ctxfin = canvas.getContext('2d');
-var ctx = offcanvas.getContext('2d');
-var contentpause = $('text');
-var gravity = 0.01;
-var stripWidth = 2;
-var blood = false;
-var panorama=true;
 
 var player = {
 	x : 5.5,		// current x, y position
@@ -703,10 +599,10 @@ var player = {
 	firerate:[125,90,130,200],
 	pierce:[0,1,1,1],
 	secondary:function(){
-		if(this.weapon===this.maxWeapon){
-			this.weapon = 1;
-		}else{
+		if(this.weapon===1){
 			this.weapon = this.maxWeapon;
+		}else{
+			this.weapon = 1;
 		}
 	},
 	primary:function(){
@@ -819,6 +715,110 @@ var player = {
 	keys:0,
 }
 
+var sprites = [
+  new Sprite(21.5,10,"lamp",false,0.6,0,0,0),
+  new Sprite(18,3,"lamp",false,0.6,0,0,0),
+  new Sprite(9.5,3.5,"lamp",false,0.6,0,0,0),
+  new Sprite(9.5,10,"lamp",false,0.6,0,0,0),
+  new Sprite(3,3.5,"lamp",false,0.6,0,0,0),
+  new Sprite(3,19.5,"lamp",false,0.6,0,0,0),
+  new Sprite(3,13,"lamp",false,0.6,0,0,0),
+  new Sprite(14,19,"lamp",false,0.6,0,0,0),
+  new Sprite(2,8,"pillar",true,0.6,1,0,0),
+  new Sprite(2,9,"pillar",true,0.6,1,0,0),
+  new Sprite(2,10,"pillar",true,0.6,1,0,0),
+  new Sprite(21,2,"barrel",true,0.6,0.4,0,0),
+  new Sprite(15.73,2.1,"barrel",true,0.6,0.4,0,0),
+  new Sprite(15.27,2.1,"barrel",true,0.6,0.4,0,0),
+  new Sprite(15.5,2.3,"barrel",true,0.6,0.4,0.4,0.4),
+  new Sprite(15.5,2.7,"barrel",true,0.6,0.4,0,0),
+  new Sprite(3,2,"barrel",true,0.6,0.4,0,0),
+  new Sprite(9,10,"barrel",true,0.6,0.4,0,0),
+  new Sprite(9.5,10,"barrel",true,0.6,0.4,0,0),
+  new Sprite(10,10,"barrel",true,0.6,0.4,0,0)
+];
+var enemies = [
+	new Enemy(15.5,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(16.0,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(16.5,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(17.0,4.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(15.5,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(16.0,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(16.5,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(17.0,3.5,0,"dog",15,Math.PI,0.075,0,true,0.5,1,true,0),
+	new Enemy(4.5,7.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(4.5,15.5,0,"ss",100,3*Math.PI/2,0.035,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
+	new Enemy(5.5,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(6.0,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(6.5,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(7.0,17.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(5.5,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(6.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(6.5,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(7.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(10.0,20.0,0,"guard",50,Math.PI,0.075,0,true,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(11.0,20.0,0,"guard",50,Math.PI,0.075,0,true,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(10.0,21.0,0,"ss",100,Math.PI,0.075,0,true,0.5,1,true,2,[['ammo',2],['smg',0]]),
+	new Enemy(11.0,21.0,0,"ss",100,Math.PI,0.075,0,true,0.5,1,true,2,[['ammo',2],['smg',0]]),
+	new Enemy(7.0,16.5,0,"dog",15,3*Math.PI/2,0.075,0,true,0.5,1,true,0),
+	new Enemy(21.5,8.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(19.5,8.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(21.5,6.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(19.5,6.5,0,"guard",50,3*Math.PI/2,0.02,0,false,0.5,1,true,1,[['ammo',2]]),
+	new Enemy(21.0,15.5,0,"ss",100,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
+	new Enemy(20.0,15.5,0,"ss",100,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0]]),
+	new Enemy(19.0,15.5,0,"ss",500,3*Math.PI/2,0.02,0,false,0.4,3,true,2,[['ammo',2],['smg',0],['bluekey',1]]),
+];
+var sounds=[['swsh_0_0','swsh_0_1'],'weapon_3','weapon_2','weapon_26'];
+var pickups = [
+	new Pickup(15.5,7.5,'chaingun',0,0,0),
+	new Pickup(15.5,7.5,'lives',0,0,4),
+	new Pickup(15.5,7.5,'goldkey',0,0,1),
+	new Pickup(15.5,3.5,'backpack',0,0,5),
+];
+var weapons_imgs = [];
+for(var texture = 0; texture < weapon_names.length;texture++){
+    var weaponTexture = new Image();
+  	weaponTexture.crossOrigin = "Anonymous";
+  	weaponTexture.src = `sprites/weapons/${weapon_names[texture]}.png`;
+    weapons_imgs.push(weaponTexture);
+}
+window.mobileAndTabletCheck = function() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+var wallTextures = [];
+var wallTypes = [0,[0,0,88],[106,0,0],[147, 147, 147],[108,69,39],[180, 179, 82],[89,0,90],[0,142,142],[0,142,142],[202,202,202],[124,124,124],[108,69,39]];
+for(var texture = 1; texture < 12;texture++){
+    var wallTexture = new Image();
+  	wallTexture.crossOrigin = "Anonymous";
+  	wallTexture.src = `walls_${texture}.png`;
+    wallTextures.push(wallTexture);
+}
+var floorTextures = []
+for(var texture = 1; texture < 11;texture++){
+    var floorTexture = new Image();
+  	floorTexture.crossOrigin = "Anonymous";
+  	floorTexture.src = `floor_${texture}.png`;
+    floorTextures.push(floorTexture);
+}
+//var floorData=getimagedata(floorTextures[1]);
+var weapon_size = screenWidth*2/3;
+mobile = window.mobileAndTabletCheck();
+var canvas = $("screen");
+var screenWidth = canvas.width;
+var screenHeight = canvas.height;
+var pickupIsPresent = false;
+var pickupNum;
+var offcanvas = new OffscreenCanvas(screenWidth,screenHeight);//document.createElement('canvas');//
+var ctxfin = canvas.getContext('2d');
+var ctx = offcanvas.getContext('2d');
+var contentpause = $('text');
+var gravity = 0.01;
+var stripWidth = 2;
+var blood = false;
+var panorama=true;
 var miniMapScale = 8;
 var doorIsPresent = false;
 var doorTarget = [0,0];
